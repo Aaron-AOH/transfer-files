a87e3cc9878856b9e9900d629de267e08569a3ec
Aaron-AOH <118374763+Aaron-AOH@users.noreply.github.com>
2023-02-27 12:35:57 +0100

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,27 @@
+import os
+
+comando = input("""
+       Escoja una opcion ...
+
+       1: view ip
+       2: upload files
+       3:
+       4:
+       5:
+       
+       >>  """)
+
+if comando ==1:
+  os.system('ipconfig')
+
+if comando == 2:
+  print("Para transferir archivos debe abrir admin_transfer.py en el OS atacante. comand = <python3 admin_transfer.py>")
+  question = input("Cuando haya realizado la tarea necesaria escriba [Y] or [y] para continuar.")
+  if question=="y" and question=="Y":
+     os.system('transfer-files.py')
+      
+if comando == 3:
+      
+      
+      
+      
